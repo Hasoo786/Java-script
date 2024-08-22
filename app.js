ae6308430b7ae910c37241f8cf0e346d10324271
Hasoo786 <hk51413121@gmail.com>
2024-08-23 04:52:41 +0500

    prompt half

---
+++ app.js
@@ -0,0 +1,104 @@
+// console.log ("Hello Java")
+
+// console.log (typeof("true"))
+
+// var fullname = "Haseeb Khan";
+
+// var age = 21;
+
+// var loggin = true;
+
+// console.log (fullname);
+
+
+
+
+// line wise likna 
+
+
+
+
+// var username = "Haseeb"
+
+// console.log (username)
+
+// var username = "Riaz"
+
+// console.log (username)
+
+// var username = "Bablu"
+
+// console.log (username)
+
+
+// barabr lana 
+
+
+
+// var num1 = 50
+// var num2 = 2
+
+// console.log ("50" +  "2")
+
+
+
+// jama krna 
+
+
+
+// var num = 10
+
+// num += 10
+
+// console.log (num)
+
+
+// true false 
+
+
+// console.log (2 > 3)
+
+// console.log (2 < 3)
+
+// console.log (2 == 2)
+
+// console.log (2 === 3)
+
+// console.log (2 != 2)
+
+// console.log (2 !== 3)
+
+// console.log (2 >= 3)
+
+// console.log (2 <= 3)
+
+
+
+// prompt practice 
+
+
+
+var eng = prompt("Enter your marks")
+
+var urdu = prompt("Enter your marks")
+
+var math = prompt("Enter your marks")
+
+var science = prompt("Enter your marks")
+
+var obtainedmarks = ( eng + urdu + math + science )
+
+var totalmarks = 400
+
+var percentage = (obtainedmarks/totalmarks*80)
+
+console.log (percentage)
+
+
+
+
+
+
+
+
+
